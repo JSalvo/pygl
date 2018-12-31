a088b517b5115c21119af02b5c2031db5c41c370
Gianmario Salvetti <gianmario.salvetti@gmail.com>
2018-12-31 09:39:55 +0100

    Implementare la selezione e pulizia codice

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from PyQt4.QtOpenGL import QGLWidget
 from OpenGL.GL import *
 from OpenGL import GLUT
 import math
+from PyQt4 import Qt
 
 
 def paint_circle(x=0, y=0, radius=1, detail=12):
@@ -58,26 +59,49 @@ class Text:
 
 class Entity:
 	def __init__(self, name):
-		self.__name = Text(name, 30, 60)
+		self._name = Text(name, 30, 60)
+		self._tx = 0
+		self._ty = 0
+		self._width = len(self._name)*104.76 + 60
+		self._height = 119.05 + 120
 
 	def get_name(self):
-		return self.__name.get_text()
+		return self._name.get_text()
 
 	def set_name(self, newName):
-		self.__name.set_text(newName)
+		self._name.set_text(newName)
+		self._width = len(self._name)*104.76 + 60
+		self._height = 119.05 + 120
+
+	def translate(self, tx, ty):
+		self._tx += tx
+		self._ty += ty
+
+	def get_selected(self, x, y):
+		if ((x >= self._tx) and (x <= self._tx + self._width) and (y >= self._ty) and (y <= self._ty + self._height)):
+			return self
+		else:
+			return None
+
+	def getWidth(self):
+		return self._width
+
+	def getHeight(self):
+		return self._height
 
 	def paint(self):
-		width = len(self.__name)*104.76 + 60
-		height = 119.05 + 120
-
+		glMatrixMode(GL_MODELVIEW)
+		glPushMatrix()
+		glTranslatef(self._tx, self._ty, 0)
 		glBegin(GL_LINE_LOOP)
 		glVertex2f(0, 0)
-		glVertex2f(width, 0)
-		glVertex2f(width, height)
-		glVertex2f(0, height)
+		glVertex2f(self._width, 0)
+		glVertex2f(self._width, self._height)
+		glVertex2f(0, self._height)
 		glEnd()
 
-		self.__name.paint()
+		self._name.paint()
+		glPopMatrix()
 
 
 class Attribute:
@@ -100,6 +124,16 @@ class GLWidget(QGLWidget):
 	def __init__(self, parent):
 		# Inizializza Antenato
 		QGLWidget.__init__(self, parent)
+		self._zoom_factor = 1.0
+		self._pan_x = 0
+		self._pan_y = 0
+		self.e = Entity("Cliente")
+
+	def panY(self, pan_y):
+		self._pan_y += pan_y
+
+	def panX(self, pan_x):
+		self._pan_x += pan_x
 
 	def initializeGL(self):
 		GLUT.glutInit([], [])
@@ -112,7 +146,7 @@ class GLWidget(QGLWidget):
 		glViewport(0, 0, self.width(), self.height())
 		glMatrixMode(GL_PROJECTION)
 		glLoadIdentity()
-		glOrtho(0.0, self.width(), 0.0, self.height(), -1.0, 1.0)
+		glOrtho(0.0 - self._pan_x * self._zoom_factor, self.width()*self._zoom_factor - self._pan_x * self._zoom_factor , 0.0 + self._pan_y * self._zoom_factor, self.height()*self._zoom_factor + self._pan_y * self._zoom_factor, -1.0, 1.0)
 
 	def paintGL(self):
 
@@ -126,8 +160,8 @@ class GLWidget(QGLWidget):
 		glLineWidth(2.0)
 		"""
 
-		e = Entity("Cliente")
-		e.paint()
+
+		self.e.paint()
 
 		#paint_circle(30, 30, 20, 12)
 
@@ -167,7 +201,7 @@ class GLWidget(QGLWidget):
 		glViewport(0, 0, self.width(), self.height())
 		glMatrixMode(GL_PROJECTION)
 		glLoadIdentity()
-		glOrtho(0.0, self.width(), 0.0, self.height(), -1.0, 1.0)
+		glOrtho(0.0 - self._pan_x * self._zoom_factor, self.width()*self._zoom_factor - self._pan_x * self._zoom_factor, 0.0 + self._pan_y * self._zoom_factor, self.height()*self._zoom_factor + self._pan_y * self._zoom_factor, -1.0, 1.0)
 
 
 class Window(QWidget):
@@ -190,12 +224,41 @@ class Window(QWidget):
 		self.setMinimumWidth(600)
 
 		self.setMinimumHeight(400)
-	def mousePressEvent(self, QMouseEvent):
-		print QMouseEvent.pos()
 
+		self.mouse_down_x = None
+		self.mouse_down_y = None
+
+		self.glWidget = glWidget
+
+	def mousePressEvent(self, QMouseEvent):
+		if QMouseEvent.buttons() & 4:
+			self.mouse_down_x = QMouseEvent.pos().x()
+			self.mouse_down_y = QMouseEvent.pos().y()
+		elif QMouseEvent.buttons() & 1:
+			r = self.glWidget.e.get_selected(QMouseEvent.pos().x(), QMouseEvent.pos().y())
+			if r != None:
+				print "Noooo! x: %d, y: %d"%(QMouseEvent.pos().x(), QMouseEvent.pos().y())
+			else:
+				print "Yeppa!"
+
+	def mouseMoveEvent(self, QMouseEvent):
+		if QMouseEvent.buttons() & 4:
+			delta_x = QMouseEvent.pos().x() - self.mouse_down_x
+			delta_y = QMouseEvent.pos().y() - self.mouse_down_y
+			print "Delta X: %d, Delta Y: %d"%(delta_x, delta_y)
+			self.glWidget.panX(delta_x)
+			self.glWidget.panY(delta_y)
+
+			self.mouse_down_x = QMouseEvent.pos().x()
+			self.mouse_down_y = QMouseEvent.pos().y()
+
+			self.glWidget.initializeGL()
+
+			self.glWidget.updateGL()
 	def mouseReleaseEvent(self, QMouseEvent):
-		cursor =QtGui.QCursor()
-		print cursor.pos()
+		if QMouseEvent.buttons() & 4:
+			cursor =QtGui.QCursor()
+			print cursor.pos()
 
 
 app = QtGui.QApplication(sys.argv)
